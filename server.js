012ae9f784a8eb14ef14f2681b9fca7076241dba
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-23 16:08:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
+const app = express();
+const { PORT } = require('./config');
 
-module.exports = function() {
-    const app = express();
-    const PORT = process.env.PORT || 3000;
-
-    app.get('/health', (req, res) => res.send('Bot is alive!'));
-
-    app.listen(PORT, () => console.log(`Keepalive server running on port ${PORT}`));
+module.exports = () => {
+  app.get('/', (req, res) => res.send('Bot is alive!'));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 };
