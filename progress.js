459720af4d95c2c522b10ef49688ad84a972a7cd
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-23 16:07:59 -0600

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,10 +1,38 @@
-function createProgressBar(current, total, size = 20) {
-    const percent = Math.min(current / total, 1);
-    const filled = Math.round(percent * size);
-    const empty = size - filled;
-    const bar = '█'.repeat(filled) + '░'.repeat(empty);
-    const percentText = Math.round(percent * 100) + '%';
-    return `${bar} ${percentText}`;
+const { getXP, updateXP } = require('./sheets');
+const { XP_RANKS, LOG_CHANNEL_ID } = require('./config');
+
+function getNextRank(xp) {
+  return XP_RANKS.find(r => r.xp > xp) || XP_RANKS[XP_RANKS.length - 1];
 }
 
-module.exports = { createProgressBar };
+function getProgressBar(currentXP, nextXP) {
+  const totalBlocks = 20;
+  const progressBlocks = Math.min(totalBlocks, Math.floor((currentXP / nextXP) * totalBlocks));
+  return '🟩'.repeat(progressBlocks) + '⬜'.repeat(totalBlocks - progressBlocks);
+}
+
+async function addXP(client, nickname, amount) {
+  const oldXP = await getXP(nickname);
+  await updateXP(nickname, amount);
+  const newXP = oldXP + amount;
+
+  const oldRank = getNextRank(oldXP);
+  const newRank = getNextRank(newXP);
+
+  // Notify if user leveled up
+  if (oldRank.name !== newRank.name && LOG_CHANNEL_ID) {
+    const channel = await client.channels.fetch(LOG_CHANNEL_ID);
+    if (channel) {
+      channel.send(`🎉 **${nickname}** has reached rank **${newRank.name}**!`);
+    }
+  }
+
+  return {
+    oldXP,
+    newXP,
+    nextRank: newRank,
+    progressBar: getProgressBar(newXP, newRank.xp)
+  };
+}
+
+module.exports = { getNextRank, getProgressBar, addXP };
