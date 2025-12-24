2b472241c608284337ed0f92034942a04ed8d0e7
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-23 19:47:55 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,8 @@
 module.exports = {
-  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+  TOKEN: process.env.DISCORD_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   GUILD_ID: process.env.GUILD_ID,
-  SHEET_ID: process.env.GOOGLE_SHEET_ID,
-  GOOGLE_JSON_B64: process.env.GOOGLE_JSON_B64,
-  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID || '',
-  PORT: process.env.PORT || 10000,
 
-  XP_RANKS: [
-    { name: "Recruit", xp: 0 },
-    { name: "Private", xp: 100 },
-    { name: "Corporal", xp: 250 },
-    { name: "Sergeant", xp: 500 },
-    { name: "Lieutenant", xp: 900 }
-  ]
+  SHEET_ID: process.env.SHEET_ID,
+  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID
 };
