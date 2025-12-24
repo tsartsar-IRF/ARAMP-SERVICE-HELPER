9ca45dbcb6be6cb95c865f265ee03a4597761d88
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-23 19:49:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
-const { DISCORD_TOKEN } = require('./config');
-const { registerCommands } = require('./commands');
-const server = require('./server');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const commands = require('./commands');
+const { TOKEN } = require('./config');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-  partials: [Partials.Channel]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.commands = new Collection();
-registerCommands(client);
+for (const cmd of commands) {
+  client.commands.set(cmd.data.name, cmd);
+}
 
-client.once('ready', async () => {
-  console.log(`${client.user.tag} is online!`);
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
   const command = client.commands.get(interaction.commandName);
   if (!command) return;
+
   try {
-    await command.execute(interaction, client);
+    await command.execute(interaction);
   } catch (err) {
     console.error(err);
-    await interaction.reply({ content: 'Error executing command.', ephemeral: true });
+    interaction.reply({ content: 'Error executing command.', ephemeral: true });
   }
 });
 
-client.login(DISCORD_TOKEN);
-server();
-
+client.login(TOKEN);
