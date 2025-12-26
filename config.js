4b9644583d70550bce4b7a7b5ec6fe9c1ed1fa1d
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-26 14:12:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+  TOKEN: process.env.DISCORD_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   GUILD_ID: process.env.GUILD_ID,
 
   SHEET_ID: process.env.SHEET_ID,
 
-  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID,
-  LOG_ROLE_ID: process.env.LOG_ROLE_ID,
-
-  // Your screenshot shows this name:
-  GOOGLE_CREDS_BASE64: process.env.GOOGLE_CREDS_BASE64,
-
-  PORT: process.env.PORT || 3000,
+  LOG_ROLE_ID: process.env.LOG_ROLE_ID, // role allowed to use /log
 };
