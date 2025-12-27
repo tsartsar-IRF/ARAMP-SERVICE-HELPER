cb40d7f3156f9235e26471d84f696fa69b72ced7
tsartsar-IRF <james.w.berni@gmail.com>
2025-12-27 06:56:08 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,11 @@
 module.exports = {
-  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
-
+  TOKEN: process.env.DISCORD_TOKEN,
   CLIENT_ID: process.env.CLIENT_ID,
   GUILD_ID: process.env.GUILD_ID,
 
   SHEET_ID: process.env.SHEET_ID,
+  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID,
+  LOG_ROLE_ID: process.env.LOG_ROLE_ID,
 
-  // keep-alive web port
   PORT: process.env.PORT || 3000,
-
-  // Google service account base64 JSON (keep the name stable)
-  GOOGLE_CREDS_ENV: "GOOGLE_CREDS_BASE64",
-
-  // optional restriction for /log
-  LOG_ROLE_ID: process.env.LOG_ROLE_ID || null,
 };
